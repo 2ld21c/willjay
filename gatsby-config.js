69b392a2f4c01d4ec5ca279667a7488fc39366bf
willjw3 <will.j.ward3@gmail.com>
2021-06-23 17:38:30 +0900

    Changed site title

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
     siteMetadata: {
-        title: `WILLIAM JAY`,
+        title: `WILL JAY`,
         description: `Website of William Jay, writer, software developer, and educator`,
         author: `willjw3`
     },
